67e4685ed4629ab936d009377ea4e2f820947b6b
rikakohira <rikako.takase88@gmail.com>
2022-09-27 22:35:37 +0900

    initial commit

---
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -0,0 +1,18 @@
+package in.techcamp.firstapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//PostControllerクラス
+public class PostController {
+    @GetMapping("/hello")
+    //showHello()メソッド
+    public String showHello(Model model){
+        var sampleText = "サンプルテキスト"
+        model.addAttribute("sampleText", sampleText);
+        //hello.htmlを呼び出し
+        return "hello";
+    }
+}
