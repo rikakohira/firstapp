7b7a232f05936a3633412b3f7c9db5669e9ccb33
rikakohira <rikako.takase88@gmail.com>
2022-09-28 15:10:49 +0900

    postFormコントローラ　設定

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 
@@ -27,5 +28,10 @@ public class PostController {
         var postList = postRepository.findAll();
         model.addAttribute("postList", postList);
         return "index";
+    }
+    @GetMapping("/postForm")
+    //データを一時保管しビューで呼び出す
+    public String showPostForm(@ModelAttribute("postForm") PostForm form){
+        return "postForm";
     }
 }
