c083d9dc02eb96d35476df30b16113b43e0db257
rikakohira <rikako.takase88@gmail.com>
2022-09-28 11:33:12 +0900

    エンティティの設定

--- src/main/java/in/techcamp/firstapp/PostEntity.java
+++ src/main/java/in/techcamp/firstapp/PostEntity.java
@@ -1,4 +1,29 @@
 package in.techcamp.firstapp;
 
 public class PostEntity {
+    private long id;
+    private String memo;
+
+    //コンストラクタ
+    public PostEntity(long id, String memo) {
+        this.id = id;
+        this.memo = memo;
+    }
+
+    //ゲッター・セッター
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
 }
