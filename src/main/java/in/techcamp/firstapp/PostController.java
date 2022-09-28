a0a70e01fe16b75ceb36c7db8426a13ed9b89a7c
rikakohira <rikako.takase88@gmail.com>
2022-09-28 11:37:28 +0900

    データ作成

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 //PostControllerクラス
 public class PostController {
@@ -17,7 +19,13 @@ public class PostController {
     }
     @GetMapping
     //showList()メソッド
-    public String showList(){
+    public String showList(Model model){
+        var postList = List.of(
+                new PostEntity(1, "投稿1"),
+                new PostEntity(2, "投稿2"),
+                new PostEntity(3, "投稿3")
+        );
+        model.addAttribute("postList", postList)
         return "index";
     }
 }
