3ea95f911da58817a5815abfca3a16ab9633bc9c
rikakohira <rikako.takase88@gmail.com>
2022-09-28 12:43:43 +0900

    ゲッター・セッター省略

--- src/main/java/in/techcamp/firstapp/PostEntity.java
+++ src/main/java/in/techcamp/firstapp/PostEntity.java
@@ -1,27 +1,13 @@
 package in.techcamp.firstapp;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 //コンストラクタを自動生成
 @AllArgsConstructor
+//ゲッター・セッターを省略
+@Data
 public class PostEntity {
     private long id;
     private String memo;
-
-    //ゲッター・セッター
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getMemo() {
-        return memo;
-    }
-
-    public void setMemo(String memo) {
-        this.memo = memo;
-    }
 }
