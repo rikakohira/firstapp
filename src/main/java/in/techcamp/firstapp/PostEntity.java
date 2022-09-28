2b7f85e3dd6b5e7b758908b2dca735b983116bd7
rikakohira <rikako.takase88@gmail.com>
2022-09-28 12:39:31 +0900

    Lombok リファクタリング

--- src/main/java/in/techcamp/firstapp/PostEntity.java
+++ src/main/java/in/techcamp/firstapp/PostEntity.java
@@ -1,14 +1,17 @@
 package in.techcamp.firstapp;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class PostEntity {
     private long id;
     private String memo;
 
-    //コンストラクタ
-    public PostEntity(long id, String memo) {
-        this.id = id;
-        this.memo = memo;
-    }
+    //コンストラクタ（リファクタリングによって削除）
+    //public PostEntity(long id, String memo) {
+        //this.id = id;
+        //this.memo = memo;
+    //}
 
     //ゲッター・セッター
     public long getId() {
